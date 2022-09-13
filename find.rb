7381cb04956182303c4195a12a10b39f5d9fcdf2
Aron <aron.0504@hotmail.com>
2022-09-12 22:00:04 -0300

    class find finished

--- find.rb
+++ find.rb
@@ -20,14 +20,46 @@ class Find
 
   def find_values
     @find.keys.each do |key|
-      value = @find[key]
-      if value["attr"].nil?
-        content = @document.at(value["find"]).content
+      if is_list?(key)
+        @values[key] = find_list(key)
       else
-        content = @document.at(value["find"]).attr(value["attr"])
+        @values[key] = find_one(key)
+      end
+    end
+  end
+
+  def is_list?(key)
+    !@find[key]["find"].nil?
+  end
+
+  def find_value(document, where, value)
+    if value.nil?
+      document.at(where).content
+    else
+      document.at(where).attr(value)
+    end
+  rescue
+    nil
+  end
+
+  def find_one(key)
+    find_value(@document, @find[key]["where"], @find[key]["attr"])
+  end
+
+  def find_list(key)
+    list_values = []
+
+    @document.search(@find[key]["where"]).each do |doc_child|
+      list_values_child = {}
+
+      @find[key]["find"].keys.each do |key_child|
+        value_child = @find[key]["find"][key_child]
+        list_values_child[key_child] = find_value(doc_child, value_child["where"], value_child["attr"])
       end
 
-      @values[key] = content
+      list_values.push(list_values_child)
     end
+
+    list_values
   end
 end
